a96cb3d51abc6f2560702405ccfd1fdc0b9be7d7
aliii390 <alli.said.errahmani@gmail.com>
2024-12-17 14:55:36 +0100

    ADD: setup font et color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: ["Poppins", "sans-serif"],
+        main: ["Inter", "sans-serif"],
+      },
+      colors : {
+        "violet": "#3a0ca3",
+      }
+    },
   },
   plugins: [],
 }
