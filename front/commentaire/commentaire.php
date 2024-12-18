8f593af045e82a7a73d2c2ba522110aa8f8e90c0
aliii390 <alli.said.errahmani@gmail.com>
2024-12-18 12:34:03 +0100

    ADD: partie responsive commentaire

---
+++ front/commentaire/commentaire.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aura.</title>
+    <link rel="stylesheet" href="../../assets/css/output.css">
+</head>
+<body class="bg-black">
+<header>
+    <h1 class="font-logo text-violet text-3xl font-bold pl-6 pt-4 mb-4 lg:text-5xl">Aura.</h1>
+</header>
+
+<main>
+    <section>
+    <article class="flex flex-col gap-3">
+        <div class="flex items-center justify-between px-3">
+            <div class="flex items-center gap-2">
+                <img src="../../GTzblVSWUAAIMp-.jpg" alt="photo de singe" class="w-10 rounded-full">
+                <p class="text-white font-sans font-medium text-sm">Pseudo-Guy</p>
+            </div>
+            <img src="../../assets/icons/doubler.png" alt="menu petits points" class="w-4">
+        </div>
+        <img src="../../GTzblVSWUAAIMp-.jpg" alt="photo d'utilisateur" class="lg:h-3/5 w-auto">
+        <div class="flex w-6 gap-2 ml-4">
+           
+        </div>
+     
+        <form action="" method="post" class="flex items-center justify-between px-3">
+            <input type="text" name="commentaire" id="commentaire" placeholder="Add a comment..." class="bg-black text-white">
+            <button type="submit" ><img src="../../assets/icons/ajouter-un-bouton.png" alt="bouton ajouter commentaire" class="w-4"></button>
+        </form>
+    </article>
+
+    <article class="flex flex-col gap-2">
+  <div class="flex items-start p-1 gap-2">
+    <img src="../../GTzblVSWUAAIMp-.jpg" alt="photo de singe" class="w-10 rounded-full">
+    <p class="text-white font-sans font-medium text-sm">
+      <span class="block font-medium">Scammeur2darons :</span>
+      <span class="font-light text-red-400">Regardez le miskine y perd l'aura</span>
+    </p>
+  </div>
+
+  <div class="flex items-start p-1 gap-2">
+    <img src="../../GTzblVSWUAAIMp-.jpg" alt="photo de singe" class="w-10 rounded-full">
+    <p class="text-white font-sans font-medium text-sm">
+      <span class="block font-medium">Arracheur2sac :</span>
+      <span class="font-light text-red-400">ah lui il a pas préparer ces 10balle mdrrr</span>
+    </p>
+  </div>
+
+  <div class="flex items-start p-1 gap-2">
+    <img src="../../GTzblVSWUAAIMp-.jpg" alt="photo de singe" class="w-10 rounded-full">
+    <p class="text-white font-sans font-medium text-sm">
+      <span class="block font-medium">SaCharbone :</span>
+      <span class="font-light text-red-400">si lui on peut on peut tous</span>
+    </p>
+  </div>
+</article>
+
+
+
+    </section>
+</main>
+
+</body>
+</html>
