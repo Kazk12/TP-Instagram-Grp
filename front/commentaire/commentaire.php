f6f43532d4dbe94c2ef7dcb0c40014750b164048
aliii390 <alli.said.errahmani@gmail.com>
2024-12-18 14:33:58 +0100

    ADD: fin de la partie commentaire

--- front/commentaire/commentaire.php
+++ front/commentaire/commentaire.php
@@ -8,7 +8,10 @@
 </head>
 <body class="bg-black">
 <header>
-    <h1 class="font-logo text-violet text-3xl font-bold pl-6 pt-4 mb-4 lg:text-5xl">Aura.</h1>
+<h1 class="font-logo text-3xl font-bold pl-6 pt-4 mb-4 lg:text-5xl bg-gradient-to-r from-blue-400 via-purple-600 to-pink-500 text-transparent bg-clip-text">
+  Aura.
+</h1>
+
 </header>
 
 <main>
@@ -21,37 +24,37 @@
             </div>
             <img src="../../assets/icons/doubler.png" alt="menu petits points" class="w-4">
         </div>
-        <img src="../../GTzblVSWUAAIMp-.jpg" alt="photo d'utilisateur" class="lg:h-3/5 w-auto">
+        <img src="../../GTzblVSWUAAIMp-.jpg" alt="photo d'utilisateur" 
+     class="w-full h-auto sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4 mx-auto">
         <div class="flex w-6 gap-2 ml-4">
            
         </div>
      
-        <form action="" method="post" class="flex items-center justify-between px-3">
-            <input type="text" name="commentaire" id="commentaire" placeholder="Add a comment..." class="bg-black text-white">
-            <button type="submit" ><img src="../../assets/icons/ajouter-un-bouton.png" alt="bouton ajouter commentaire" class="w-4"></button>
-        </form>
+        
     </article>
 
-    <article class="flex flex-col gap-2">
-  <div class="flex items-start p-1 gap-2">
+
+    
+    <article class="flex flex-col gap-2 xl:justify-center px-4 md:px-6 lg:px-8">
+  <div class="flex items-start p-2 gap-3 sm:p-3 md:p-4">
     <img src="../../GTzblVSWUAAIMp-.jpg" alt="photo de singe" class="w-10 rounded-full">
-    <p class="text-white font-sans font-medium text-sm">
+    <p class="text-white font-sans font-medium text-sm sm:text-base md:text-lg">
       <span class="block font-medium">Scammeur2darons :</span>
       <span class="font-light text-red-400">Regardez le miskine y perd l'aura</span>
     </p>
   </div>
 
-  <div class="flex items-start p-1 gap-2">
+  <div class="flex items-start p-2 gap-3 sm:p-3 md:p-4">
     <img src="../../GTzblVSWUAAIMp-.jpg" alt="photo de singe" class="w-10 rounded-full">
-    <p class="text-white font-sans font-medium text-sm">
+    <p class="text-white font-sans font-medium text-sm sm:text-base md:text-lg">
       <span class="block font-medium">Arracheur2sac :</span>
-      <span class="font-light text-red-400">ah lui il a pas préparer ces 10balle mdrrr</span>
+      <span class="font-light text-red-400">ah lui il a pas préparé ces 10balle mdrrr</span>
     </p>
   </div>
 
-  <div class="flex items-start p-1 gap-2">
+  <div class="flex items-start p-2 gap-3 sm:p-3 md:p-4">
     <img src="../../GTzblVSWUAAIMp-.jpg" alt="photo de singe" class="w-10 rounded-full">
-    <p class="text-white font-sans font-medium text-sm">
+    <p class="text-white font-sans font-medium text-sm sm:text-base md:text-lg">
       <span class="block font-medium">SaCharbone :</span>
       <span class="font-light text-red-400">si lui on peut on peut tous</span>
     </p>
@@ -59,6 +62,11 @@
 </article>
 
 
+
+<form action="" method="post" class="flex items-center justify-between px-3">
+            <input type="text" name="commentaire" id="commentaire" placeholder="Ajoutez un commentaire" class="bg-black text-white">
+            <button type="submit" ><img src="../../assets/icons/ajouter-un-bouton.png" alt="bouton ajouter commentaire" class="w-4"></button>
+        </form>
 
     </section>
 </main>
