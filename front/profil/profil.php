3c4b25ca3d43d1522ce6889c3cd90b0d288351be
aliii390 <alli.said.errahmani@gmail.com>
2024-12-20 09:10:21 +0100

    ADD: test

--- front/profil/profil.php
+++ front/profil/profil.php
@@ -49,6 +49,26 @@ $pseudoProfil = $_SESSION["pseudo"]["pseudo"];
         </nav>
     </header>
 
+
+    <section>
+<nav class="hidden lg:flex lg:justify-center lg:gap-16 lg:items-center lg:px-4 lg:fixed   ">
+        <!-- Icône de Test -->
+         <a href="../accueil/accueil.php">
+         <img src="../../assets/icons/Test.png" alt="test icon" class="lg:w-6 lg:h-10 lg:object-contain">
+         </a>
+
+        <!-- Icône Loupe (recherche) -->
+        <img src="../../assets/icons/Loupe_1.png" alt="search icon" class="lg:w-6  lg:h-10 lg:object-contain">
+
+        <!-- Icône Ajouter (post) -->
+        <img src="../../assets/icons/ajouter-un-bouton.png" alt="add button" class="lg:w-6  lg:h-12 lg:object-contain">
+
+        <!-- Photo de l'utilisateur (profil) avec un lien -->
+        <a href="../profil/profil.php">
+            <img src="../../GTzblVSWUAAIMp-.jpg" alt="photo de profil" class="lg:w-10  lg:h-10  lg:rounded-full  lg:object-cover">
+        </a>
+    </nav>
+</section>
  
 
     <section class="flex flex-col items-center ">
